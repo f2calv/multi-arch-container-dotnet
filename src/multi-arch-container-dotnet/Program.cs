57c10e486cfafe367f70bd3c489f779232dedb4a
Alex Vincent <alexv@microsoft.com>
2023-01-11 10:46:18 +0100

    create holder classes

--- src/multi-arch-container-dotnet/Program.cs
+++ src/multi-arch-container-dotnet/Program.cs
@@ -14,17 +14,36 @@ while (true)
 {
     Log.Information("App '{appName}' on [Process Architecture: {arch}, OSArchitecture: {osArch}, OSDescription: {os}].",
         AppDomain.CurrentDomain.FriendlyName, RuntimeInformation.ProcessArchitecture, RuntimeInformation.OSArchitecture, RuntimeInformation.OSDescription);
-
-    Log.Information("Repository information; name '{GIT_REPO}', branch '{GIT_BRANCH}', commit '{GIT_COMMIT}', tag '{GIT_TAG}'",
-        Environment.GetEnvironmentVariable("GIT_REPO"),
-        Environment.GetEnvironmentVariable("GIT_BRANCH"),
-        Environment.GetEnvironmentVariable("GIT_COMMIT"),
-        Environment.GetEnvironmentVariable("GIT_TAG"));
-
-    Log.Information("CI/CD information; GitHub Workflow '{GITHUB_WORKFLOW}', run id '{GITHUB_RUN_ID}', run number '{GITHUB_RUN_NUMBER}'",
-        Environment.GetEnvironmentVariable("GITHUB_WORKFLOW"),
-        Environment.GetEnvironmentVariable("GITHUB_RUN_ID"),
-        Environment.GetEnvironmentVariable("GITHUB_RUN_NUMBER"));
+    Log.Information("Git information; {@gitInfo}", new GitInfo());
+    Log.Information("Github information; {@gitHubInfo}", new GitHubInfo());
 
     await Task.Delay(2_000, cancellationToken: CancellationToken.None);
+}
+
+public class GitInfo
+{
+    public GitInfo()
+    {
+        GIT_REPO = Environment.GetEnvironmentVariable("GIT_REPO") ?? "n/a";
+        GIT_BRANCH = Environment.GetEnvironmentVariable("GIT_BRANCH") ?? "n/a";
+        GIT_COMMIT = Environment.GetEnvironmentVariable("GIT_COMMIT") ?? "n/a";
+        GIT_TAG = Environment.GetEnvironmentVariable("GIT_TAG");
+    }
+    public string? GIT_REPO { get; }
+    public string? GIT_BRANCH { get; }
+    public string? GIT_COMMIT { get; }
+    public string? GIT_TAG { get; }
+}
+
+public class GitHubInfo
+{
+    public GitHubInfo()
+    {
+        GITHUB_WORKFLOW = Environment.GetEnvironmentVariable("GIT_REPO") ?? "n/a";
+        GITHUB_RUN_ID = Environment.GetEnvironmentVariable("GIT_BRANCH") ?? "n/a";
+        GITHUB_RUN_NUMBER = Environment.GetEnvironmentVariable("GIT_COMMIT") ?? "n/a";
+    }
+    public string? GITHUB_WORKFLOW { get; }
+    public string? GITHUB_RUN_ID { get; }
+    public string? GITHUB_RUN_NUMBER { get; }
 }
