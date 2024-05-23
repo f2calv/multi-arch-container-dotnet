602acfd73af6ed5b5a069761a457b1116ea40c90
Alex Vincent <alexv@microsoft.com>
2024-05-23 14:57:39 +0200

    F2calv/2024 05 env var updates (#39)
    * GIT_REPO -> GIT_REPOSITORY
    
    * tidy
    
    * add env vars

--- build.ps1
+++ build.ps1
@@ -3,14 +3,14 @@ $ErrorActionPreference = "Stop"
 
 #set variables to emulate running in the workflow/pipeline
 $REPO_ROOT = git rev-parse --show-toplevel
-$GIT_REPO = $REPO_ROOT | Split-Path -Leaf
-$GIT_TAG = "latest-dev"
+$GIT_REPOSITORY = $REPO_ROOT | Split-Path -Leaf
 $GIT_BRANCH = $(git branch --show-current)
 $GIT_COMMIT = $(git rev-parse HEAD)
+$GIT_TAG = "latest-dev"
 $GITHUB_WORKFLOW = "n/a"
 $GITHUB_RUN_ID = 0
 $GITHUB_RUN_NUMBER = 0
-$IMAGE_NAME = "$($GIT_REPO):$($GIT_TAG)"
+$IMAGE_NAME = "$($GIT_REPOSITORY):$($GIT_TAG)"
 #Note: you cannot export a buildx container image into a local docker instance with multiple architecture manifests so for local testing you have to select just a single architecture.
 #$PLATFORM = "linux/amd64,linux/arm64,linux/arm/v7"
 $PLATFORM = "linux/amd64"
@@ -23,11 +23,10 @@ docker buildx create --name multiarchcontainerdotnet --use
 #https://github.com/docker/buildx/blob/master/docs/reference/buildx_build.md
 docker buildx build `
     -t $IMAGE_NAME `
-    -t "$($GIT_REPO):latest" `
-    --build-arg GIT_REPO=$GIT_REPO `
-    --build-arg GIT_TAG=$GIT_TAG `
+    --build-arg GIT_REPOSITORY=$GIT_REPOSITORY `
     --build-arg GIT_BRANCH=$GIT_BRANCH `
     --build-arg GIT_COMMIT=$GIT_COMMIT `
+    --build-arg GIT_TAG=$GIT_TAG `
     --build-arg GITHUB_WORKFLOW=$GITHUB_WORKFLOW `
     --build-arg GITHUB_RUN_ID=$GITHUB_RUN_ID `
     --build-arg GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER `
@@ -38,13 +37,13 @@ docker buildx build `
 
 #Preview matching images
 #https://docs.docker.com/engine/reference/commandline/images/
-docker images $GIT_REPO
+docker images $GIT_REPOSITORY
 
 Write-Host "Hit ENTER to run the '$IMAGE_NAME' image..." 
 pause
 
 #Run the multi-architecture container image
 #https://docs.docker.com/engine/reference/commandline/run/
-docker run --rm -it --name $GIT_REPO $IMAGE_NAME
+docker run --rm -it --name $GIT_REPOSITORY $IMAGE_NAME
 
 #kubectl run -i --tty --attach multi-arch-container-dotnet --image=ghcr.io/f2calv/multi-arch-container-dotnet --image-pull-policy='Always'
--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 #set variables to emulate running in the workflow/pipeline
-GIT_REPO=$(basename `git rev-parse --show-toplevel`)
+GIT_REPOSITORY=$(basename `git rev-parse --show-toplevel`)
 GIT_BRANCH=$(git branch --show-current)
 GIT_COMMIT=$(git rev-parse HEAD)
 GIT_TAG="latest-dev"
 GITHUB_WORKFLOW="n/a"
 GITHUB_RUN_ID=0
 GITHUB_RUN_NUMBER=0
-IMAGE_NAME="$GIT_REPO:$GIT_TAG"
+IMAGE_NAME="$GIT_REPOSITORY:$GIT_TAG"
 #Note: you cannot export a buildx container image into a local docker instance with multiple architecture manifests so for local testing you have to select just a single architecture.
 #$PLATFORM="linux/amd64,linux/arm64,linux/arm/v7"
 PLATFORM="linux/amd64"
@@ -21,13 +21,12 @@ docker buildx create --name multiarchcontainerdotnet --use
 #https://github.com/docker/buildx/blob/master/docs/reference/buildx_build.md
 docker buildx build \
     -t $IMAGE_NAME \
-    -t "$GIT_REPO:latest" \
     --label "GITHUB_RUN_ID=$GITHUB_RUN_ID" \
     --label "IMAGE_NAME=$IMAGE_NAME" \
-    --build-arg GIT_REPO=$GIT_REPO \
-    --build-arg GIT_TAG=$GIT_TAG \
+    --build-arg GIT_REPOSITORY=$GIT_REPOSITORY \
     --build-arg GIT_BRANCH=$GIT_BRANCH \
     --build-arg GIT_COMMIT=$GIT_COMMIT \
+    --build-arg GIT_TAG=$GIT_TAG \
     --build-arg GITHUB_WORKFLOW=$GITHUB_WORKFLOW \
     --build-arg GITHUB_RUN_ID=$GITHUB_RUN_ID \
     --build-arg GITHUB_RUN_NUMBER=$GITHUB_RUN_NUMBER \
@@ -38,13 +37,13 @@ docker buildx build \
 
 #Preview matching images
 #https://docs.docker.com/engine/reference/commandline/images/
-docker images $GIT_REPO
+docker images $GIT_REPOSITORY
 
 read -p "Hit ENTER to run the '$IMAGE_NAME' image..."
 
 #Run the multi-architecture container image
 #https://docs.docker.com/engine/reference/commandline/run/
-docker run --rm -it --name $GIT_REPO $IMAGE_NAME
+docker run --rm -it --name $GIT_REPOSITORY $IMAGE_NAME
 
 #userprofile=$(wslpath "$(wslvar USERPROFILE)")
 #export KUBECONFIG=$userprofile/.kube/config
