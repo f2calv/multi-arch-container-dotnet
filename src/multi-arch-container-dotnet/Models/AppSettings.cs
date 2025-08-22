e913eec1d334c6aba1df22fa4db9f4bae47e1a65
Alex Vincent <alex@far2cool.com>
2025-08-22 05:49:37 +0200

    tidy c#

---
+++ src/multi-arch-container-dotnet/Models/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿namespace CasCap.Models;
+
+public class AppSettings
+{
+    public AppSettings()
+    {
+        GIT_REPOSITORY = Environment.GetEnvironmentVariable(nameof(GIT_REPOSITORY)) ?? "n/a";
+        GIT_BRANCH = Environment.GetEnvironmentVariable(nameof(GIT_BRANCH)) ?? "n/a";
+        GIT_COMMIT = Environment.GetEnvironmentVariable(nameof(GIT_COMMIT)) ?? "n/a";
+        GIT_TAG = Environment.GetEnvironmentVariable(nameof(GIT_TAG)) ?? "n/a";
+        GITHUB_WORKFLOW = Environment.GetEnvironmentVariable(nameof(GITHUB_WORKFLOW)) ?? "n/a";
+        GITHUB_RUN_ID = Environment.GetEnvironmentVariable(nameof(GITHUB_RUN_ID)) ?? "n/a";
+        GITHUB_RUN_NUMBER = Environment.GetEnvironmentVariable(nameof(GITHUB_RUN_NUMBER)) ?? "n/a";
+    }
+    public string? GIT_REPOSITORY { get; }
+    public string? GIT_BRANCH { get; }
+    public string? GIT_COMMIT { get; }
+    public string? GIT_TAG { get; }
+    public string? GITHUB_WORKFLOW { get; }
+    public string? GITHUB_RUN_ID { get; }
+    public string? GITHUB_RUN_NUMBER { get; }
+}
